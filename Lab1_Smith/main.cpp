2f9fa8f9840804404771af1ec12bd4f42c65eb93
samuelsmithjr <samuel.smith7@rockets.utoledo.edu>
2024-04-04 15:17:37 -0400

    Initial Commit

---
+++ Lab1_Smith/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab1_Smith
+//
+//  Created by Sam Smith Jr. on 4/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
